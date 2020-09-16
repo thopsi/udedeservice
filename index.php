5bbef3658917c1d56baf5bca834748e02c7499d3
thopsi <71377220+thopsi@users.noreply.github.com>
2020-09-16 17:15:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = ''; 
-$channelSecret = '';
+$ACCESS_TOKEN = 'fMVGvZiqW0XP2WFyDRfh3yR72TQZ8+tV08ifTlXhCv7C+AXjthPuXrij91Jc8JfDTMSk7Ky/Ll4WyZ6PYBsGWx/usYJBHH+uH/wW40pOHGuaB0rSJmW6znOplGBuqQbShHfzoOui12J1v8+R5pSMHAdB04t89/1O/w1cDnyilFU=
+'; 
+$channelSecret = '5b1d353ce167ed400e969c2f2989360c';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
