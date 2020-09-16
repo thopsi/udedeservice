ede8bc4f426d4d410388892fafd11fa427db2606
thopsi <71377220+thopsi@users.noreply.github.com>
2020-09-16 17:51:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,7 +33,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
- 
+
+echo "OK";
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
